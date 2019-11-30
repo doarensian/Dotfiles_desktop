365b9545eebf5a666b2c1ad4e4844c84fc20511d
doarensian <doarensian@gmail.com>
2019-11-30 23:37:04 +0900

    Fix zsh setup.sh

--- .vim/setup.sh
+++ .vim/setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 #vim options 
-mkdir -p ~/dotfiles_desktop/.vim/autoload ~/dotfiles_desktop/.vim/bundle && \
-curl -LSso ~/dotfiles_desktop/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/Dotfiles_desktop/.vim/autoload ~/Dotfiles_desktop/.vim/bundle && \
+curl -LSso ~/Dotfiles_desktop/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 sleep 3
-git clone git clone https://github.com/edkolev/tmuxline.vim ~/dotfiles_desktop/.vim/bundle/tmuxline.vim
+git clone https://github.com/edkolev/tmuxline.vim ~/Dotfiles_desktop/.vim/bundle/tmuxline.vim
 sleep 3
-git clone https://github.com/vim-airline/vim-airline ~/dotfiles_desktop/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline ~/Dotfiles_desktop/.vim/bundle/vim-airline
 sleep 3
-git clone https://github.com/vim-airline/vim-airline-themes ~/dotfiles_desktop/.vim/bundle/vim-airline-themes
+git clone https://github.com/vim-airline/vim-airline-themes ~/Dotfiles_desktop/.vim/bundle/vim-airline-themes
 sleep 3
-git clone https://github.com/easymotion/vim-easymotion ~/dotfiles_desktop/.vim/bundle/vim-easymotion
+git clone https://github.com/easymotion/vim-easymotion ~/Dotfiles_desktop/.vim/bundle/vim-easymotion
--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 
 Installation:
 
-
     git clone https://github.com/doarensian/Dotfiles_desktop.git ~
     cd ~/Dotfiles_desktop
     
     If you use ~/Dotfiles_desktop/.vimrc, Just Below commands
 
         $ ln -sf ~/Dotfiles_desktop/.vimrc ~/
+
+Configuration:
+
+    If you use vim options(like vim-airline,vim-easymotion,....), Just below commands.
+    
+        $ cd ~/Dotfiles_desktop/.vim/
+        $ ./setup.sh
+
+    And also vim plugins auto update commands.
+
+        $ cd ~/Dotfiles_desktop/.vim/
+        $ ./update.sh
 
 ![Alt text](./images/screenshot.png?raw=true "Title")
